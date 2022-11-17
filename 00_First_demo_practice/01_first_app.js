ac2faba64925c81b92491efae7bb3e34b704b44a
Ramireddy Chandra sekhar reddy <ramireddychandu77@gmail.com>
2022-11-17 19:12:04 +0530

    Basic Practice starter

---
+++ 00_First_demo_practice/01_first_app.js
@@ -0,0 +1,11 @@
+// Testing the NodeJS Terminal : Openthe terminal and node filename.js
+// Code-1
+// console.log('Hello This is JavaScript File Pls look Carefully ...')
+
+// Code-2 (create a new file and write the content inside ...)
+// const fs = require('fs');
+// fs.writeFileSync('01_File_system_write_fun.txt','Hello this is nodejs pls help me ...')
+
+//  There are two types of running Nodejs Code
+//  => REPL ->read ->eval ->print ->loop  => In cmd press node and Activation start.
+//  => node filename.js in terminal of VSCODE.
